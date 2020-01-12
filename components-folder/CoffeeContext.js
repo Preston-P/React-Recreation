0951bd2ffacf6fb9dd28d32b1f78ce4c4b8c8641
Preston-P <34893141+Preston-P@users.noreply.github.com>
2020-01-12 11:33:44 -0800

    displaying an array of descr
    need to access and display child component whether through  props or something... still trying to figure out

--- components-folder/CoffeeContext.js
+++ components-folder/CoffeeContext.js
@@ -11,45 +11,50 @@ function CoffeeContext() {
   ]);
 
   const handleSubmit = event => {
-    alert(`your favorite flavor is ${coffees.type}`);
-
-    console.log("Handle submit has occured");
-    console.log(coffees.type);
-    console.log(coffees.descr);
-
-    console.log(filterTaste);
+    // console.log(coffees.type);
     event.preventDefault();
   };
 
   const handleChange = event => {
-    setCoffees({ type: event.target.value });
-    console.log("Handle change has occured");
+    //setCoffees({ type: event.target.value });
+    event.persist();
+
+    setCoffees(prevCoffees => [...prevCoffees, { type: event.target.value }]);
+
+    event.target.value === "Chocolate"
+      ? console.log(event.target.value)
+      : console.log("not Chocolate");
+
+    const filterTaste = coffees
+      .filter(item => item.type === event.target.value)
+      .map(item => <ul key={item.type}>{item.descr}</ul>);
+
+    console.log(filterTaste);
   };
 
-  const handleOutput = () => {
-    let numb = 3;
-    console.log(numb);
-  };
-  const filterTaste = coffees
+  /*   const filterTaste = coffees
     .filter(coffee => coffee.type === "Chocolate")
-    .map(coffee => <div>{coffee.descr}</div>);
+    .map(coffee => <div>{coffee.descr}</div>); */
+
+  /* What i want to do with filter taste is to reach 
+    all the items of type and desr and set the target value and filter that
+    and then map to the specific descr of the coffees .type
+    might need to use spread operator
+         
+    */
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        <Menu onChange={handleChange} value={(coffees.type, coffees.descr)}>
+        <Menu onChange={handleChange} value={coffees.type}>
           <option value="Fruity">Fruity</option>
-          <option value={coffees.descr}>Biscuit/Malt</option>
+          <option value="Biscuit">Biscuit/Malt</option>
           <option value="Chocolate">Chocolate</option>
           <option value="Earthy">Earthy</option>
         </Menu>
         <input type="submit" value="submit"></input>
       </label>
-      <div>
-        {" "}
-        -Flavor profile to be placed here once submitted-
-        {filterTaste}
-      </div>
+      <div></div>
     </form>
   );
 }
